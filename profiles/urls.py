80a5744b4b4e2fbabc2262672dde6a0c71345351
Yemo <113440465+yemoalakija@users.noreply.github.com>
2023-09-29 19:39:11 +0000

    Create CRUD

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from profiles import views
 
 urlpatterns = [
     path("profiles/", views.ProfileList.as_view()),
+    path("profiles/<int:primary_key>/", views.ProfileDetail.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,6 @@
 """Profiles views."""
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -14,3 +16,36 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()  # pylint: disable=no-member
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """Profile detail."""
+    serializer_class = ProfileSerializer
+    def get_object(self, primary_key):
+        """Get object."""
+        try:
+            profile = Profile.objects.get(pk=primary_key)  # pylint: disable=no-member
+            return profile
+        except Profile.DoesNotExist as exc:  # pylint: disable=no-member
+            raise Http404 from exc
+
+    def get(self, request, primary_key):
+        """Get."""
+        profile = self.get_object(primary_key)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, primary_key):
+        """Put."""
+        profile = self.get_object(primary_key)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, primary_key):
+        """Delete."""
+        profile = self.get_object(primary_key)
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
