65ea1f4dc71a239ccfc7c319a754a2422aa50466
Yemo <113440465+yemoalakija@users.noreply.github.com>
2023-10-01 17:33:10 +0000

    Update serializers, dt and settings

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,4 +1,5 @@
 """Comments serializers."""
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -9,21 +10,23 @@ class CommentSerializer(serializers.ModelSerializer):
 
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.SerializerMethodField(source="owner.profile.id")
-    profile_image = serializers.SerializerMethodField(source="owner.profile.image.url")
+    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
+    profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         """Get is owner."""
         request = self.context["request"]
         return request.user == obj.owner
 
-    def get_profile_id(self, obj):
-        """Get profile id."""
-        return obj.owner.profile.id
+    def get_created_at(self, obj):
+        """Get created at."""
+        return naturaltime(obj.created_at)
 
-    def get_profile_image(self, obj):
-        """Get profile image."""
-        return obj.owner.profile.image.url
+    def get_updated_at(self, obj):
+        """Get updated at."""
+        return naturaltime(obj.updated_at)
 
     class Meta:
         """Meta class."""
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -28,8 +28,16 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [(
         "rest_framework.authentication.SessionAuthentication"
         if os.environ.get("DEV") == "1" else "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
-    )]
+    )],
+    "DEFAULT_PAGINATION_CLASS":
+        "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DATETIME_FORMAT": "%d %b %Y",
 }
+if "DEV" not in os.environ:
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
+        "rest_framework.renderers.JSONRenderer",
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
