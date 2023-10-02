70872b7ea62cbdd4179f8f0a3554272034533692
Yemo <113440465+yemoalakija@users.noreply.github.com>
2023-10-02 08:48:33 +0000

    Cors

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
 import dotenv
 
@@ -105,14 +106,15 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-if "CLIENT_ORIGIN" in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ["CLIENT_ORIGIN"],
-    ]
-else:
-    CORS_ALLOWED_ORIGINS = [
-        r"^https://.*\.github\.dev$",
-    ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-',
+        os.environ.get('CLIENT_ORIGIN_DEV', ''),
+    )
+    if extracted_url:
+        CORS_ALLOWED_ORIGIN_REGEXES = [
+            f"{extracted_url.group(0)}\\w+\\.github\\.dev$",
+        ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
