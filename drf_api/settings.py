1a09f0521b3e6ecab68b4e46dde643f4a8170e36
yemoalakija <>
2023-10-10 11:25:44 +0000

    Allow render

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -86,6 +86,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://3000-yemoalakija-dishcovery-i1fxkcthde.us2.codeanyapp.com",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://dishcovery.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
